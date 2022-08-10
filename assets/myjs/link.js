e5d2da3a79038012e32ae34f26275b5c89e85627
luckypatcherx <59965177+luckypatcherx@users.noreply.github.com>
2022-08-10 14:36:44 +0530

    update planet 3
    update

---
+++ assets/myjs/link.js
@@ -0,0 +1,19 @@
+function unique(){
+    window.location.replace("index.html#unique");  
+}
+function values(){
+    window.location.replace("index.html#values");
+}
+function services(){
+    window.location.replace("index.html#services"); 
+}
+function products(){
+    window.location.replace("index.html#products")
+}
+function values(){
+    window.location.replace("index.html#values")
+}
+
+
+
+
