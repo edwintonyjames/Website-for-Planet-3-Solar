14900c9938adb819efa081eca1358aad977737e5
edwintonyjames <edwintonyjames8@gmail.com>
2022-08-13 12:35:06 +0530

    pop up added

---
+++ assetz/js/popup.js
@@ -0,0 +1,15 @@
+const btn_sub_close = document.getElementById("subs-close");
+const blr = document.getElementById("mncntr");
+const mncntr = document.getElementById("mncntr");
+
+
+
+
+setTimeout (function(){
+    mncntr.classList.add("ppupblr")
+    document.getElementsByClassName("popup-main")[0].style.display = "flex"
+}, 5000);
+
+btn_sub_close.addEventListener("click", () => {
+    document.getElementsByClassName("popup-main")[0].style.display = "none"
+    blr.classList.remove("ppupblr");});
