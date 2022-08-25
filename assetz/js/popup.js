4b12a3e0bc32f1eaad9d95d159e388b7cf6adf89
Edwin Tony James <edwintonyjames8@gmail.com>
2022-08-25 11:01:19 +0530

    a

--- assetz/js/popup.js
+++ assetz/js/popup.js
@@ -2,8 +2,7 @@ const btn_sub_close = document.getElementById("subs-close");
 const blr = document.getElementById("mncntr");
 const mncntr = document.getElementById("mncntr");
 
-
-
+//popupfadein
 
 setTimeout (function(){
     mncntr.classList.add("ppupblr")
@@ -13,3 +12,5 @@ setTimeout (function(){
 btn_sub_close.addEventListener("click", () => {
     document.getElementsByClassName("popup-main")[0].style.display = "none"
     blr.classList.remove("ppupblr");});
+
+   
